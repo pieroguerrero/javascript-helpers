56fca39b98e330203ef301419489937f05d6a443
Piero G <piero.guerrero@gmail.com>
2022-01-27 19:37:32 -0500

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+
+  return arr.reduce((prev, curr) => {
+    return prev + curr;
+  }, 0);
+
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  if (arr.length > 0) {
+    return arr.reduce((prev, curr) => {
+      return prev * curr;
+    });
+  } else return 0;
+
 
 };
 
-const power = function() {
-	
+const power = function (x, e) {
+  return x ** e;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  else if (n > 0) {
+    return n * factorial(n - 1);
+  } else return "ERROR";
+
 };
+
+//sum([]);
 
 // Do not edit below this line
 module.exports = {
